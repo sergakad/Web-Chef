50ceaeef6f52e9ae52b35e0196cad83d302fc2dc
sergakad <kadsereg@gmail.com>
2025-02-26 21:06:14 +0300

    work result with state

--- src/components/CategoryCardSection/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCardSection/CategoryCard/CategoryCard.tsx
@@ -19,6 +19,7 @@ const CategoryCard: FC<ICategoryCard> = ({
   const handleMouseEnter = () => {
     setHovered(true);
   };
+  
   const handleMouseLeave = () => {
     setTimeout(() => {
       setHovered(false);
@@ -32,24 +33,21 @@ const CategoryCard: FC<ICategoryCard> = ({
         s[`cardWrapperHovered_${isHovered}`],
       )}
     >
+      <Link className={s.link} href={`/categories/${name}`}>
+        <div
+          className={s.card}
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+          }}
+        >
+          <h3 className={s.name}>{name}</h3>
+        </div>
+      </Link>
       <div
         className={s.descriptionWrapper}
         onMouseLeave={handleMouseLeave}
         onMouseEnter={handleMouseEnter}
       >
-        <Link
-          className={s.link}
-          href={`/categories/${name}`}
-        >
-          <div
-            className={s.card}
-            style={{
-              backgroundImage: `url(${backgroundImage})`,
-            }}
-          >
-            <h3 className={s.name}>{name}</h3>
-          </div>
-        </Link>
         <span className={s.description}>{description}</span>
         <div className={s.barrier} />
       </div>
